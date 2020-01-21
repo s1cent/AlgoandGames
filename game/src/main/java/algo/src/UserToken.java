accc1fc49de04418059a55a217906adfb46aab07
Lukas Pucher <lukas.pucher@student.tugraz.at>
2020-01-21 13:32:46 +0100

    Push with my Token

--- game/src/main/java/algo/src/UserToken.java
+++ game/src/main/java/algo/src/UserToken.java
@@ -2,5 +2,5 @@ package algo.src;
 
 // TODO: Make a file called UserToken.java and copy the content with your personal token
 public class UserToken {
-    static String userToken = "6c99de592aa78419870cdf98e6f287bffb5c70f48ac29f8284010a02849ecb93";       // TODO: Adapt for your own token
+    static String userToken = "5458cde0230944b8569628e25b7d7f9f0f1a3c83de236addc3aa219ff06c6320";       // TODO: Adapt for your own token
 }
