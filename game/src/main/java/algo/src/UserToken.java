15e2a4961f898aad56fcef66774ebb59e42d08b1
Lukas Pucher <lukas.pucher@student.tugraz.at>
2020-01-21 13:20:30 +0100

    WTF was this...

---
+++ game/src/main/java/algo/src/UserToken.java
@@ -0,0 +1,6 @@
+package algo.src;
+
+// TODO: Make a file called UserToken.java and copy the content with your personal token
+public class UserToken {
+    static String userToken = "6c99de592aa78419870cdf98e6f287bffb5c70f48ac29f8284010a02849ecb93";       // TODO: Adapt for your own token
+}
