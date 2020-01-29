4165cf090f9ae2338e03c443caba698405e56e93
Stefan Lang <s.lang@student-tugraz.at>
2020-01-29 14:36:14 +0100

    User

--- game/src/main/java/algo/src/UserRegistrationSettings.java
+++ game/src/main/java/algo/src/UserRegistrationSettings.java
@@ -2,8 +2,8 @@ package algo.src;
 
 public class UserRegistrationSettings {
     // TODO: DO NOT COMMIT CHANGES TO THIS FILE TO GIT
-    static String email = "s.lang@student.tugraz.at";                                       // TODO: Personalize this ...@student.tugraz.at
-    static String full_name = "Stefan Lang";                                   // TODO: Personalize this
-    static String matrNr = "01611738";                                      // TODO: Personalize this
+    static String email = "";                                       // TODO: Personalize this ...@student.tugraz.at
+    static String full_name = "";                                   // TODO: Personalize this
+    static String matrNr = "";                                      // TODO: Personalize this
     static String secret = "AS19pseudogangPass";                    // TODO: Recommended: Leave this as is
 }
